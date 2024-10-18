9c3564a7e87ad8a0c95642cb1c5afdca44224c23
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-10-18 14:09:58 +0200

    Palindroma - add comments

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -1,3 +1,12 @@
-// ! Palidroma
+// ! Palindroma
 // * Chiedere all’utente di inserire una parola
 // * Creare una funzione per capire se la parola inserita è palindroma
+
+// # Elaborazione
+// creo una funzione
+// chiedo all'utente con un prompt di inserire una parola
+// verifico che sia palindroma
+// imposto un return
+// chiudo funzione
+
+// messaggio di output con alert
