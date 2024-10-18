6233fe73a47f19a4d00308c3c52dc86f5de5d272
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-10-18 15:35:54 +0200

    Palindroma - elaborazione e output

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -3,10 +3,38 @@
 // * Creare una funzione per capire se la parola inserita è palindroma
 
 // # Elaborazione
-// creo una funzione
-// chiedo all'utente con un prompt di inserire una parola
-// verifico che sia palindroma
-// imposto un return
-// chiudo funzione
+/**
+ * funzione che chiede all'utente una parola e controlla che sia palindroma
+ * @returns {string}  se la parola inserita è palindroma o no
+ */
+function isUserWordPalindrome() {
+  // chiedi all'utente di inserire una parola
+  const userWord = prompt("Inserisci una parola");
 
-// messaggio di output con alert
+  // dividi la parola in lettere e inseriscila in un array
+  let lettersUserWord = userWord.split("");
+
+  // inverti l'ordine delle lettere
+  lettersUserWord = lettersUserWord.reverse();
+
+  // riporta le lettere in parola
+  const invertedUserWord = lettersUserWord.join("");
+
+  // controlla se la parola inserita dall'utente e uguale a se stessa inverita
+  let isPalindrome = invertedUserWord === userWord;
+
+  // SE la parola è palindroma
+  if (isPalindrome) {
+    // stampo messagio di output
+    return `E' una parola palindroma`;
+  }
+
+  // ALTRIMENTI
+  else {
+    // stampo messagio di output
+    return `Non è una parola palindroma`;
+  }
+}
+
+// # Output
+alert(isUserWordPalindrome());
